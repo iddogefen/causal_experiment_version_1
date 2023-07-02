e2b2b49ae322abbbd88780279e7573a3fe40bd15
iddo gefen <iddo.gefen@gmail.com>
2023-07-02 03:35:23 -0400

    First commit

--- Tools/Functions.js
+++ Tools/Functions.js
@@ -135,7 +135,7 @@ function save_local_data(name, data){
 function save_local_data(filename, data){
   $.ajax({
     type: 'POST',
-    url: 'http://localhost:8000/Tools/write_data_local.php', // Replace this URL with the URL of your PHP script
+    url: '34.125.186.26/Tools/write_data_local.php', // Replace this URL with the URL of your PHP script
     data: {
       data: data,
       filename: filename
