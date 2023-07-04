c8e1445d29682c77913aab2bd8bc9c9a25fb83ef
iddo gefen <iddo.gefen@gmail.com>
2023-07-04 00:24:21 -0400

    First commit

--- Tools/Functions.js
+++ Tools/Functions.js
@@ -111,14 +111,14 @@ function transpose(a) {
 
 function saveData(name, data){
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'write_data.php'); // 'write_data.php' is the path to the php file described above.
+  xhr.open('POST', 'write_data_1.php'); // 'write_data.php' is the path to the php file described above.
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(JSON.stringify({filedata: data}));
 }
 
 // call the saveData function after the experiment is over
 initJsPsych({
-   on_finish: function(){ saveData(jsPsych.data.get().csv()); }
+   on_finish: function(){ saveData(jsPsych.data.get().JSON()); }
 });
 
 // Save data to file functions
