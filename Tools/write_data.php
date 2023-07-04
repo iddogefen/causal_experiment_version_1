45b27bbeec93b153ee648241e8a3ab102507d14c
iddo gefen <iddo.gefen@gmail.com>
2023-07-04 04:11:29 -0400

    First commit

--- Tools/write_data.php
+++ Tools/write_data.php
@@ -1,5 +1,6 @@
 <?php
 // get the data from the POST message
+echo "yes";
 $post_data = json_decode(file_get_contents('php://input'), true);
 $data = $post_data['filedata'];
 // generate a unique ID for the file, e.g., session-6feu833950202 
