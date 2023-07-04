a7af17b9e48bd98952a156a162370b91e74def82
iddo gefen <iddo.gefen@gmail.com>
2023-07-04 04:20:55 -0400

    First commit

--- Tools/write_data.php
+++ Tools/write_data.php
@@ -1,14 +1,8 @@
 <?php
-// get the data from the POST message
-echo "yes";
 $post_data = json_decode(file_get_contents('php://input'), true);
-$data = $post_data['filedata'];
-// generate a unique ID for the file, e.g., session-6feu833950202 
-$file = uniqid("session-");
 // the directory "data" must be writable by the server
-$name = "Data/{$file}.json";
-echo "File saved successfully.";
-
+$name = "../Data/".$post_data['filename'].".jason";
+$data = $post_data['filedata'];
 // write the file to disk
 file_put_contents($name, $data);
 ?>
