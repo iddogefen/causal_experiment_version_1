db863971c20b480d5206ace7e696fe2bbed76619
iddo gefen <iddo.gefen@gmail.com>
2023-07-04 03:35:17 -0400

    First commit

--- Tools/Functions.js
+++ Tools/Functions.js
@@ -133,6 +133,14 @@ function save_server_data(name, data) {
   }));
 }
 
+
+function saveData(name, data){
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'write_data.php'); // 'write_data.php' is the path to the php file described above.
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({filedata: data}));
+}
+
 /*
 
 function save_local_data(name, data){
