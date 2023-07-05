5e4786c523274f4214d35b9533170909d5b98eb5
iddo gefen <iddo.gefen@gmail.com>
2023-07-05 17:25:53 -0400

    5

--- Tools/write_data.php
+++ Tools/write_data.php
@@ -1,9 +1,10 @@
 <?php
 $post_data = json_decode(file_get_contents('php://input'), true);
 // the directory "data" must be writable by the server
-$name = "data/".$post_data['filename'].".jason";
+$name = "../data/".$post_data['filename'].".jason";
 $data = $post_data['filedata'];
 // write the file to disk
 file_put_contents($name, $data);
 echo "File saved successfully 2.";
 ?>
+
