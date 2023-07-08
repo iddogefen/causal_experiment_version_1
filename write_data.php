940def6f2f0c9d022d920cbb71db80177d9c0436
iddo gefen <iddo.gefen@gmail.com>
2023-07-08 00:53:47 -0400

    1_27

--- write_data.php
+++ write_data.php
@@ -3,7 +3,7 @@
 $post_data = json_decode(file_get_contents('php://input'), true);
 $data = $post_data['filedata'];
 // generate a unique ID for the file, e.g., session-6feu833950202 
-$file = uniqid("session-");
+$file = date("Y-m-d h:i:sa");
 // the directory "data" must be writable by the server
 $name = "data/{$file}.json"; 
 // write the file to disk
